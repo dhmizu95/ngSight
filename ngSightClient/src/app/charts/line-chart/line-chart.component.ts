297a0acf4b9eaffc404b202522ed597a89f5c1ea
MIzu Rahman <dhmizu95@gmail.com>
2019-07-11 21:56:17 +0600

    Add line chart component data.

--- ngSightClient/src/app/charts/line-chart/line-chart.component.ts
+++ ngSightClient/src/app/charts/line-chart/line-chart.component.ts
@@ -1,4 +1,13 @@
+import { LINECHART_COLOR } from '../../shared/chart.colors';
 import { Component, OnInit } from '@angular/core';
+
+const SAMPLE_LINECHART_DATA = [
+  { data: [45, 57, 50, 41, 56, 55], label: 'Sentiment Analysis' },
+  { data: [28, 48, 40, 19, 46, 27], label: 'Image Recognition' },
+  { data: [28, 54, 40, 55, 45, 54], label: 'Forcasting' }
+];
+
+const SAMPLE_LINECHART_LABLE = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'];
 
 @Component({
   selector: 'app-line-chart',
@@ -6,10 +15,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./line-chart.component.css']
 })
 export class LineChartComponent implements OnInit {
+  public lineChartData: any[] = SAMPLE_LINECHART_DATA;
+  public lineChartLabels: string[] = SAMPLE_LINECHART_LABLE;
+  public lineChartOptions: any = {
+    responsive: true
+  };
+  public lineChartLegend = true;
+  public lineChartType = 'line';
+  public lineChartColors = LINECHART_COLOR;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
