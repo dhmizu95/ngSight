f9aa8251f0cca75d1d2a624c0cd1730e8e39f14d
MIzu Rahman <dhmizu95@gmail.com>
2019-07-11 21:15:49 +0600

    Add pie chart component data.

--- ngSightClient/src/app/charts/pie-chart/pie-chart.component.ts
+++ ngSightClient/src/app/charts/pie-chart/pie-chart.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pie-chart.component.css']
 })
 export class PieChartComponent implements OnInit {
+  public pieChartData: number[] = [345, 109, 578];
+  public pieChartLabels: string[] = [
+    'XYX Logistics',
+    'Abul & Co.',
+    'Chapter 14'
+  ];
+  public pieChartColors: any[] = [
+    {
+      backgroundColor: ['#26547c', '#ff6b6b', '#ffd166'],
+      borderColor: '#111111'
+    }
+  ];
+  public pieChartType = 'doughnut';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
