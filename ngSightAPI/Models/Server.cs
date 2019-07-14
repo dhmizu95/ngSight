56ea020d798ec869ab84715f56ac7ceb34ed3bc2
MIzu Rahman <dhmizu95@gmail.com>
2019-07-14 18:32:10 +0600

    Add models, DbContext to Server project

---
+++ ngSightAPI/Models/Server.cs
@@ -0,0 +1,9 @@
+﻿namespace ngSightAPI.Models
+{
+    public class Server
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsOnline { get; set; }
+    }
+}
