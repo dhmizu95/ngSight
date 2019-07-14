c43e29e87626ae0ebcadfcc5419d011c503941fd
MIzu Rahman <dhmizu95@gmail.com>
2019-07-14 20:29:41 +0600

    Add database connection to serrver.

--- ngSightAPI/Models/ApiContext.cs
+++ ngSightAPI/Models/ApiContext.cs
@@ -4,12 +4,12 @@ namespace ngSightAPI.Models
 {
     public class ApiContext : DbContext
     {
-        public ApiContext(DbContextOptions<ApiContext> options) 
+        public ApiContext(DbContextOptions<ApiContext> options)
             : base(options)
         { }
 
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Order> Orders { get; set; }
         public DbSet<Server> Servers { get; set; }
-        }
+    }
 }
--- ngSightAPI/Startup.cs
+++ ngSightAPI/Startup.cs
@@ -1,13 +1,16 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using ngSightAPI.Models;
 
 namespace ngSightAPI
 {
     public class Startup
     {
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -18,11 +21,18 @@ namespace ngSightAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddEntityFrameworkNpgsql()
+                .AddDbContext<ApiContext>(opt =>
+                    opt.UseNpgsql((Configuration.GetConnectionString("DefaultConnection"))));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app,
+            IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
