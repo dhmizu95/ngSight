3d6ef583bf05f10f710e4adf7d5f7b44936d14b7
MIzu Rahman <dhmizu95@gmail.com>
2019-07-20 21:43:36 +0600

    Add cors policy.

--- ngSightAPI/Startup.cs
+++ ngSightAPI/Startup.cs
@@ -23,6 +23,11 @@ namespace ngSightAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", 
+                    c => c.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin().AllowCredentials());
+            });
 
             services.AddMvc()
                 .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
@@ -39,6 +44,7 @@ namespace ngSightAPI
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseCors("CorsPolicy");
             }
             else
             {
